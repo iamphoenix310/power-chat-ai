3e6bd14f7d7e70316f8ed1e0c5b09857cbe51fa6
Vadim Savin <savinvadim1312@gmail.com>
2025-06-05 17:17:54 +0200

    Markdown

--- src/components/MessageListItem.tsx
+++ src/components/MessageListItem.tsx
@@ -1,4 +1,65 @@
 import { Text, View } from 'react-native';
+import Markdown from 'react-native-markdown-display';
+
+const markdownStyles = {
+  body: {
+    color: 'white',
+  },
+  code_inline: {
+    backgroundColor: '#1e1e1e',
+    color: 'white',
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  code_block: {
+    backgroundColor: '#1e1e1e',
+    color: 'white',
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  fence: {
+    backgroundColor: '#1e1e1e',
+    color: 'white',
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  blockquote: {
+    backgroundColor: '#2d2d2d',
+    borderLeftColor: '#4d4d4d',
+    borderLeftWidth: 4,
+    paddingLeft: 16,
+    paddingVertical: 8,
+    marginVertical: 8,
+  },
+  bullet_list: {
+    marginVertical: 8,
+  },
+  ordered_list: {
+    marginVertical: 8,
+  },
+  list_item: {
+    marginVertical: 4,
+  },
+  hr: {
+    backgroundColor: '#4d4d4d',
+    marginVertical: 16,
+  },
+  heading1: {
+    marginVertical: 10,
+  },
+  heading2: {
+    marginVertical: 10,
+  },
+  heading3: {
+    marginVertical: 10,
+  },
+};
 
 export default function MessageListItem({ messageItem }) {
   const { message, role } = messageItem;
@@ -11,9 +72,11 @@ export default function MessageListItem({ messageItem }) {
       }`}
     >
       <View
-        className={`rounded-2xl p-4 ${isUser && 'bg-[#262626] max-w-[70%]'}`}
+        className={`rounded-2xl p-4 py-1 ${
+          isUser && 'bg-[#262626] max-w-[70%]'
+        }`}
       >
-        <Text className='text-white'>{messageItem.message}</Text>
+        <Markdown style={markdownStyles}>{messageItem.message}</Markdown>
       </View>
     </View>
   );
