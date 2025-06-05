a1a143b06a16c7db807da8f8a78d54eec4db3e1e
Vadim Savin <savinvadim1312@gmail.com>
2025-06-05 15:54:07 +0200

    setup expo router

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='flex-1 items-center justify-center bg-red-500'>
+      <Text className='text-3xl text-white font-bold'>Home 123</Text>
+    </View>
+  );
+}
