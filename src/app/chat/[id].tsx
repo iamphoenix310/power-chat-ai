83ff8bdd12a3a6a5fb3fb8101e7657c2da7d835a
Vadim Savin <savinvadim1312@gmail.com>
2025-06-05 16:34:24 +0200

    chat screen

--- src/app/chat/[id].tsx
+++ src/app/chat/[id].tsx
@@ -1,12 +1,24 @@
 import { View, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
+import chatHistory from '@assets/data/chatHistory.json';
+
 export default function ChatScreen() {
   const { id } = useLocalSearchParams();
 
+  const chat = chatHistory.find((chat) => chat.id === id);
+
+  if (!chat) {
+    return (
+      <View>
+        <Text>Chat {id} not found</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text className='text-white'>Chat Screen: {id}</Text>
+      <Text className='text-white'>Chat Screen: {chat.title}</Text>
     </View>
   );
 }
