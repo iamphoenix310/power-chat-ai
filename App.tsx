8411b6b352e89a52a9706d734994c4d5f2b87659
Vadim Savin <savinvadim1312@gmail.com>
2025-06-05 15:40:32 +0200

    setup nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <View className='flex-1 bg-white dark:bg-black items-center justify-center'>
+      <Text className='text-3xl text-black dark:text-white font-bold'>
+        Hello world
+      </Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
